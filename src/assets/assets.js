d7ef4b16b03d2016eb3b85051536d8b165f309eb
AnishKumar100 <akcoder1102004@gmail.com>
2025-06-13 13:46:45 +0530

    add navbar

---
+++ src/assets/assets.js
@@ -0,0 +1,14 @@
+import logoDark from "./logoDark.webp"
+import darkLogo from "./darkLogo.png"
+import whiteLogo from "./whiteLogo.png"
+
+import React from 'react'
+
+
+
+ export const assets={
+        logoDark,
+        darkLogo,
+        whiteLogo
+    }
+ 
